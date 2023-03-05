abb2c160a8d7dca56d8beac9d47c1735a9b6c13b
1ser1n1 <91410657+1ser1n1@users.noreply.github.com>
2023-03-05 20:27:52 +0900

    42576 코드 수정

--- 프로그래머스/42576 완주하지못한선수.py
+++ 프로그래머스/42576 완주하지못한선수.py
@@ -1,9 +1,11 @@
 def solution(participant, completion):
     sumHash = 0
+    hashDict = {}
     
     # 1. Hash : Participant의 dictionary 만들기
     # 2. Participant의 sum(hash) 구하기
     for part in participant:
+        hashDict[hash(part)] = part
         sumHash += hash(part)
     
     # 3. completion의 sum(hash) 빼기
@@ -11,5 +13,4 @@ def solution(participant, completion):
         sumHash -= hash(comp)
     
     # 4. 남은 값이 완주하지 못한 선수의 hash 값이 된다
-
     return hashDict[sumHash]
