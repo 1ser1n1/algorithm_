82e2fe484aa213c77d88f3c4ed94438327ea2b0f
1ser1n1 <91410657+1ser1n1@users.noreply.github.com>
2023-03-19 00:08:05 +0900

    update 11003최솟값찾기.py

--- 백준/11003 최솟값찾기.py
+++ 백준/11003 최솟값찾기.py
@@ -8,11 +8,17 @@ mydeque = deque() #deque 자료구조 활용
 now = list(map(int, input().split()))
 
 for i in range(N): 
-    if mydeque and mydeque[-1][0] > now[i]: #deque안의 마지막 값이 지금 넣으려는 값보다 크면 deque 마지막 값 삭제
+    while mydeque and mydeque[-1][0] > now[i]: #deque안의 마지막 값이 지금 넣으려는 값보다 크면 deque 마지막 값 삭제
         mydeque.pop()
-    mydeque.append([now[i], i])
+    mydeque.append((now[i], i))
     
-    if mydeque[-1][1] - L >= mydeque[0][1]: #슬라이딩 박스에 포함되지 않으면 삭제
+    if i - L >= mydeque[0][1]: #현재 인덱스를 기준으로 슬라이딩 박스에 포함되지 않으면 삭제
         mydeque.popleft()
         
     print(mydeque[0][0], end = " ")
+    
+    
+    """
+    문제풀면서 느낀 것
+    set과 list의 append속도 차이난다. 수정 사항이 없을 때는 set을 권장
+    """
