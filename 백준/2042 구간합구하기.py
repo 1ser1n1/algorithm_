988691c130419e655dda30cf82aefacebc085372
1ser1n1 <91410657+1ser1n1@users.noreply.github.com>
2023-03-19 15:34:11 +0900

    update 2042 구간합구하기.py

--- 백준/2042 구간합구하기.py
+++ 백준/2042 구간합구하기.py
@@ -1,5 +1,7 @@
+"""
+시간초과 -> 세그먼트 트리로 풀어야함
+"""
 import sys
-
 input = sys.stdin.readline
 
 N, M, K = map(int, input().split())
@@ -18,14 +20,15 @@ for i in range(0, N):
     else:    
         list_sum[i] = list_sum[i-1] + l[i]
 
-print(list_sum)
+#구간합 수정 및 출력
 for i in range(M+K):
     a, b, c = map(int, input().split())
+    #구간합 수정
     if a == 1:
-        m = c - l[b-1]
+        m = c - l[b-1] #현재 값과 바꿀 값의 오차
         l[b-1] = c
-        for j in range(b-1, N):
-            list_sum[j] = list_sum[j] + m
-            
+        for j in range(b-1, N): #바꾼 위치부터 오차를 더해줌
+            list_sum[j] = list_sum[j] + m 
+    #조건에 맞는 구간합 출력       
     else : 
         print(list_sum[c-1]-list_sum[b-1]+l[b-1])
